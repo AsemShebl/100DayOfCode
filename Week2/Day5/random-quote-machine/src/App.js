cf5411e0cc847712a1eb15b5ad61b426164248b7
AsemShebl <asemalaashebl@gmail.com>
2021-01-12 07:37:48 +0200

    100 DAY OF Code WEEK2:5 DAY12: Random Quote machine | Create the Button Component

--- Week2/Day5/random-quote-machine/src/App.js
+++ Week2/Day5/random-quote-machine/src/App.js
@@ -1,23 +1,22 @@
-import logo from "./logo.svg";
+import React, { Component } from "react";
 import "./App.css";
+import Button from "./components/Button";
 
-function App() {
-  return (
-    <div className="App" id="quote-box">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello, I'm Asem!</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  nextQuoteClickHandler() {
+    console.log("h1");
+  }
+
+  render() {
+    return (
+      <div className="App" id="quote-box">
+        <Button
+          buttonDisplayNone="Next Quote"
+          onClick={this.nextQuoteClickHandler}
+        />
+      </div>
+    );
+  }
 }
 
 export default App;
