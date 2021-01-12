73d6cb2861b6521b2a51a7ad90348014f55a73a6
AsemShebl <asemalaashebl@gmail.com>
2021-01-12 11:33:06 +0200

    100 DAY OF Code WEEK2:5 DAY12: Random Quote Machine | Add Material Design to React with Material UI

--- Week2/Day5/random-quote-machine/src/App.js
+++ Week2/Day5/random-quote-machine/src/App.js
@@ -1,7 +1,16 @@
 import React, { Component } from "react";
 import { random } from "lodash";
-import "./App.css";
+import "typeface-roboto";
+import { Grid, withStyles } from "@material-ui/core";
 import QuotesMachine from "./components/QuoteMachine";
+
+const styles = {
+  container: {
+    alignItems: "center",
+    display: "flex",
+    height: "100vh",
+  },
+};
 
 class App extends Component {
   constructor(props) {
@@ -47,14 +56,21 @@ class App extends Component {
 
   render() {
     return (
-      <div className="App" id="quote-box">
-        <QuotesMachine
-          selectedQuote={this.selectedQuote}
-          assignNewQuoteIndex={this.assignNewQuoteIndex}
-        />
-      </div>
+      <Grid
+        className={this.props.classes.container}
+        id="quote-box"
+        justify="center"
+        container
+      >
+        <Grid xs={11} lg={8} item>
+          <QuotesMachine
+            selectedQuote={this.selectedQuote}
+            assignNewQuoteIndex={this.assignNewQuoteIndex}
+          />
+        </Grid>
+      </Grid>
     );
   }
 }
 
-export default App;
+export default withStyles(styles)(App);
--- Week2/Day5/random-quote-machine/src/components/Button.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const Button = ({ buttonDisplayNone, clickHandler }) => (
-  <button onClick={clickHandler}>{buttonDisplayNone}</button>
-);
-
-export default Button;
