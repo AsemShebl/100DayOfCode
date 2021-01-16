311490d0a378e837327c42bb0cb0032df5734688
AsemShebl <asemalaashebl@gmail.com>
2021-01-16 19:16:12 +0200

    100 DAY OF Code WEEK3:2 DAY16 | Pomodoro Clock

---
+++ Week3/Day2/pomodoro-clock/src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Break from "./components/Break";
+import "./App.css";
+import Session from "./components/Session";
+
+function App() {
+  return (
+    <div className="App">
+      <Break />
+      <Session />
+    </div>
+  );
+}
+
+export default App;
